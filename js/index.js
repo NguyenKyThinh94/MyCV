235ec9aa4a65a778abbf84654ee060295800d9e3
= <=>
2019-10-20 18:09:29 +0700

    Fix content

--- js/index.js
+++ js/index.js
@@ -2,15 +2,9 @@ let moreProject1 = document.getElementById('moreProject1');
 let moreProject2 = document.getElementById('moreProject2');
 let moreProject3 = document.getElementById('moreProject3');
 
-
 moreProject1.addEventListener('click',loadMoreProject);                     
 moreProject2.addEventListener('click',loadMoreProject);                     
 moreProject3.addEventListener('click',loadMoreProject);                     
-
-
-
-
-
 
 function loadMoreProject(){
     let id=this.id;
@@ -23,13 +17,11 @@ function loadMoreProject(){
     
 }
 function Reder(data,id){
- 
     for(i=0;i<=2;i++){
         let query="#project"+ (i+1).toString();
         let btquery="#moreProject"+ (i+1).toString();
         let element = document.querySelector(query);
-        let button =document.querySelector(btquery);
-        console.log(button);
+        let button =document.querySelector(btquery);     
         if(i==id){            
             if(element.innerHTML.length < 5){
                 button.setAttribute('class','fas fa-minus-circle d-inline-block mx-2 cursorAlias');
@@ -46,7 +38,5 @@ function Reder(data,id){
             element.innerHTML="";
             button.setAttribute('class','fas fa-plus-circle mx-2 d-block cursorCopy')
         }
-    }
-   
-    
+    }   
 }
